b0020eb98cb921519abbdddd0eef452e2095c850
Jack Sheriff <jacksheriff@gmail.com>
2015-01-27 23:42:56 -0500

    Read from multiple files for each data type. Add initial Greek cities to data.

--- loader/loader.py
+++ loader/loader.py
@@ -1,8 +1,16 @@
 import csv
 import json
+import os
 from collections import namedtuple
 
 print("civitates-data loader starting")
+
+# TBD: Read the data locations from a file
+base_path = "../data"
+city_base_files = ["Italy/CityBase.txt", "Greece/CityBase.txt"]
+period_base_files = ["Italy/PeriodBase.txt", "Greece/PeriodBase.txt"]
+alt_name_base_files = ["Italy/AltNameBase.txt", "Greece/AltNameBase.txt"]
+city_extra_files = ["Italy/CityExtra.txt"]
 
 # Read geonames file
 GeonamesCity = namedtuple("GeonamesCity", "city_id name ascii_name alternate_names latitude longitude feature_class feature_code country_code cc2 admin_code1 admin_code2 admin_code3 admin_code4 population elevation dem time_zone modification_date")
@@ -12,11 +20,14 @@ with open("../data/cities1000.txt", encoding="utf8") as file:
 
 print("Read", len(geonames_city_list), "rows from cities1000.txt")
 
-# Read CityBase.txt
+# Read CityBase files
 CityBase = namedtuple("CityBase", "id geonames_name geonames_cc prefix")
-with open("../data/CityBase.txt", encoding="utf8") as file:
-    reader = csv.reader(file, delimiter='\t', quoting=csv.QUOTE_NONE)
-    city_base_list = [CityBase._make(x) for x in reader]
+city_base_list = []
+for filename in city_base_files:
+    file_path = os.path.join(base_path, filename)
+    with open(file_path, encoding="utf8") as file:
+        reader = csv.reader(file, delimiter='\t', quoting=csv.QUOTE_NONE)
+        city_base_list.extend([CityBase._make(x) for x in reader])
 
 print("Read", len(city_base_list), "rows from CityBase.txt")
 
@@ -29,25 +40,34 @@ def count_id(id, list):
 
 # Read PeriodBase.txt
 PeriodBase = namedtuple("PeriodBase", "id start_date end_date preferredName size tag_position")
-with open("../data/PeriodBase.txt", encoding="utf8") as file:
-    reader = csv.reader(file, delimiter='\t', quoting=csv.QUOTE_NONE)
-    period_base_list = [PeriodBase._make(x) for x in reader]
+period_base_list = []
+for filename in period_base_files:
+    file_path = os.path.join(base_path, filename)
+    with open(file_path, encoding="utf8") as file:
+        reader = csv.reader(file, delimiter='\t', quoting=csv.QUOTE_NONE)
+        period_base_list.extend([PeriodBase._make(x) for x in reader])
 
 print("Read", len(period_base_list), "rows from PeriodBase.txt")
 
 # Read AltNameBase.txt
 AltNameBase = namedtuple("AltNameBase", "id alt_name language")
-with open("../data/AltNameBase.txt", encoding="utf8") as file:
-    reader = csv.reader(file, delimiter='\t', quoting=csv.QUOTE_NONE)
-    alt_name_base_list = [AltNameBase._make(x) for x in reader]
+alt_name_base_list = []
+for filename in alt_name_base_files:
+    file_path = os.path.join(base_path, filename)
+    with open(file_path, encoding="utf8") as file:
+        reader = csv.reader(file, delimiter='\t', quoting=csv.QUOTE_NONE)
+        alt_name_base_list.extend([AltNameBase._make(x) for x in reader])
 
 print("Read", len(alt_name_base_list), "rows from AltNamedBase.txt")
 
 # Read CityExtra.txt
 CityExtra = namedtuple("CityExtra", "id latitude longitude elevation")
-with open("../data/CityExtra.txt", encoding="utf8") as file:
-    reader = csv.reader(file, delimiter='\t', quoting=csv.QUOTE_NONE)
-    city_extra_list = [CityExtra._make(x) for x in reader]
+city_extra_list = []
+for filename in city_extra_files:
+    file_path = os.path.join(base_path, filename)
+    with open(file_path, encoding="utf8") as file:
+        reader = csv.reader(file, delimiter='\t', quoting=csv.QUOTE_NONE)
+        city_extra_list.extend([CityExtra._make(x) for x in reader])
 
 print("Read", len(city_extra_list), "rows from CityExtra.txt")
 
